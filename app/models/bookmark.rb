cb4c0b3e5134ad87232b706d04fc5c70edaeae42
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-01-02 16:49:33 +0900

    Change rooting

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,11 @@ class Bookmark < ApplicationRecord
   before_save :fetch_title
 
   def fetch_title
-    puts 'hello fetch_title'
-    self.title = 'sample'
+    uri = URI.parse(url)
+    response = Net::HTTP.get(uri).force_encoding(Encoding::UTF_8)
+    captures = /<title>(?<title>.+)<\/title>/m.match(response).named_captures
+    self.title = captures['title']
+  rescue => e
+    logger.warn "Fetch Title Faild. url = [#{url}], error = #{e.message}"
   end
 end
